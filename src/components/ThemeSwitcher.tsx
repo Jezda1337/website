178887d33473af9e09f2f7fb509a1b689e6255c6
Radoje <megasrbin1@gmail.com>
2023-01-11 21:02:52 +0100

    optimize code using $

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,4 +1,4 @@
-import { component$, useClientEffect$ } from "@builder.io/qwik";
+import { component$, useClientEffect$, $ } from "@builder.io/qwik";
 
 interface Props {
   state: {
@@ -31,16 +31,18 @@ export default component$(({ state }: { state: Props["state"] }) => {
         document.body.classList.remove("darkDots"));
   });
 
+  const handleTheme = $(() => {
+    state.isDark = !state.isDark;
+    state.isDark ? (state.theme = "dark") : (state.theme = "light");
+    localStorage.theme = JSON.stringify(state.theme);
+    console.log("hello world");
+  });
+
   return (
     <>
       <button
         aria-label="switch the theme"
-        onClick$={() => {
-          state.isDark = !state.isDark;
-          state.isDark ? (state.theme = "dark") : (state.theme = "light");
-          localStorage.theme = JSON.stringify(state.theme);
-          console.log("hello world");
-        }}
+        onClick$={handleTheme}
         class={`w-16 bg-slate-300 flex justify-between rounded relative after:absolute after:w-1/2 after:h-full after:dark:bg-slate-800 after:bg-white after:rounded h-8 items-center px-1 gap-3 shadow-[inset_0_0px_4px_rgba(0,0,0,0.6)]  ${
           state.isDark
             ? "after:left-1/2 bg-slate-600 after:transition-all"
