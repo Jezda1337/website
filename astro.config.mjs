fb4817defa571ee343c2a16331405ec5247818e4
Radoje <megasrbin1@gmail.com>
2023-08-25 01:00:03 +0200

    adding cloudflare adapter :tada:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
-import { defineConfig } from "astro/config"
-import sitemap from "@astrojs/sitemap"
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	trailingSlash: "never",
-	site: "https://radoje.dev/",
-	integrations: [tailwind(), sitemap()],
-	experimental: {
-		viewTransitions: true,
-	},
-	markdown: {
-		// Example: Switch to use prism for syntax highlighting in Markdown
-		syntaxHighlight: "prism",
-	},
-	output: "server",
-})
-
+  trailingSlash: "never",
+  site: "https://radoje.dev/",
+  integrations: [tailwind(), sitemap()],
+  experimental: {
+    viewTransitions: true
+  },
+  markdown: {
+    // Example: Switch to use prism for syntax highlighting in Markdown
+    syntaxHighlight: "prism"
+  },
+  output: "server",
+  adapter: cloudflare()
+});
