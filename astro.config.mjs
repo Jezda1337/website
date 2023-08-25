325126995a145866610ec6de6874f10f8ee90ea6
Radoje <megasrbin1@gmail.com>
2023-08-25 15:17:13 +0200

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,5 +17,5 @@ export default defineConfig({
     syntaxHighlight: "prism"
   },
   output: "hybrid",
-  adapter: cloudflare()
+  // adapter: cloudflare()
 });
