1405a90d3fcfbe5ebf632da1016ce6f96e3bbaa9
Radoje <megasrbin1@gmail.com>
2023-08-26 01:34:26 +0200

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from "@astrojs/cloudflare"
 // https://astro.build/config
 export default defineConfig({
 	trailingSlash: "never",
-	site: "https://radoje.dev/",
+	site: "https://radoje.dev",
 	integrations: [tailwind(), sitemap()],
 	experimental: {
 		viewTransitions: true,
