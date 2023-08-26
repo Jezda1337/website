d6b55e117a096a28be33adde635c520ea818b443
Radoje <megasrbin1@gmail.com>
2023-08-26 22:39:37 +0200

    fix broken build :fire:

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-	trailingSlash: "never",
+	// trailingSlash: "never",
 	site: "https://radoje.dev/",
 	integrations: [tailwind(), sitemap()],
 	experimental: {
