ce730a2bdbc3d8133ae6841d93e1b21f32d4469f
Radoje <megasrbin1@gmail.com>
2023-08-29 18:11:12 +0200

    fixing redirection 308 problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-	trailingSlash: "never",
-	site: "https://radoje.dev/",
+	// trailingSlash: "never",
+	site: "https://radoje.dev",
 	integrations: [tailwind(), sitemap()],
 	experimental: {
 		viewTransitions: true,
