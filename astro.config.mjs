c7839b9820fc7dad52daa458df5a92a112b5dc4d
Radoje <megasrbin1@gmail.com>
2023-08-30 17:23:43 +0200

    updating to astro 3.0, and dependencies to the latest, remove unused code, and delete unused pacakges

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,14 @@ import { defineConfig } from "astro/config"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 
-// import cloudflare from "@astrojs/cloudflare"
-
 // https://astro.build/config
 export default defineConfig({
-	// trailingSlash: "never",
-	site: "https://radoje.dev",
-	integrations: [tailwind(), sitemap()],
-	experimental: {
-		viewTransitions: true,
-	},
-	markdown: {
-		syntaxHighlight: "prism",
-	},
-	build: {
-		format: "file",
-	},
-	// output: "server",
-	// adapter: cloudflare(),
-	// server: {
-	// 	headers: {
-	// 		"Cache-Control": "public, max-age=86400, immutable",
-	// 	},
-	// },
+  site: "https://radoje.dev",
+  integrations: [tailwind(), sitemap()],
+  markdown: {
+    syntaxHighlight: "prism",
+  },
+  build: {
+    format: "file",
+  },
 })
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,22 +1,22 @@
 /** @type {import("prettier").Config} */
 
 module.exports = {
-	singleQuote: false,
-	semi: false,
-	useTabs: true,
-	tabWidth: 2,
-	arrowParens: "always",
-	bracketSpacing: true,
-	bracketSameLine: true,
-	insertPragma: false,
-	singleAttributePerLine: true,
-	plugins: ["prettier-plugin-astro"],
-	overrides: [
-		{
-			files: "*.astro",
-			options: {
-				parser: "astro",
-			},
-		},
-	],
+  singleQuote: false,
+  semi: false,
+  useTabs: true,
+  tabWidth: 2,
+  arrowParens: "always",
+  bracketSpacing: true,
+  bracketSameLine: true,
+  insertPragma: false,
+  singleAttributePerLine: true,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 }
